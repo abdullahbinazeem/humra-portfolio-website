d0fda5c9c8c38efbdd24970b2235a840a0ad685d
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2023-09-21 22:48:25 -0700

    set datasets

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -3,5 +3,5 @@ import { createClient } from "next-sanity";
 export const client = createClient({
   apiVersion: "2023-03-04",
   projectId: "8at4agk2",
-  dataset: "sanity",
+  dataset: "production",
 });
