e1c2fb596afecb817d59a0ac105ab8bba31cb342
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2023-09-21 22:46:21 -0700

    fixed some more stuff maybe ?

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,22 +1,18 @@
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\app\admin\[[...index]]\page.tsx` route
- */
-
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import schemas from "@/sanity/schemas";
 
 export default defineConfig({
-  basePath: "/admin",
-
   name: "default",
   title: "sanity-tut",
+
   apiVersion: "2023-03-04",
   projectId: "8at4agk2",
   dataset: "sanity",
+  basePath: "/admin",
+
   schema: { types: schemas },
   plugins: [deskTool(), visionTool({ defaultApiVersion: "2023-03-04" })],
 });
